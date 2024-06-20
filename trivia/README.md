c9968f4d8a0337b3709e97391097922c504e2fcb
eric-sclafani <eric.sclafani321@gmail.com>
2024-06-20 13:52:26 -0400

    added api url generation

--- trivia/README.md
+++ trivia/README.md
@@ -1,3 +1,5 @@
 # Trivia app
 
 - Uses [this api](https://opentdb.com/api_config.php) to get questions
+
+- Might host on [Render](https://render.com/)
--- trivia/src/index.ts
+++ trivia/src/index.ts
@@ -1,10 +1,6 @@
 import { parseCategoryData } from "./categories.js"
 
 
-
-
-
-
 async function populateCategoryDropdown ():Promise<void>{
     await parseCategoryData();
     const categoryDropdown = $("select[name='category']");
@@ -19,14 +15,47 @@ async function populateCategoryDropdown ():Promise<void>{
 
         const value = categoryIDs[categoryNames.indexOf(name)].toString();
         option.value = value;
-
         categoryDropdown.append(option)
-    }
+    } 
+}
 
+const getAmount = (value:string):string => {
+    return `amount=${value}`;
+}
 
+const getCategory = (value:string):string => {
+    return value == "any" ? "" : `&category=${value}`;
+}
+
+const getDifficulty = (value:string):string => {
+    return value == "any" ? "" : `&difficulty=${value}`;
+}
+
+const getType = (value:string):string => {
+    return value == "any" ? "" : `&type=${value}`;
+}
+
+const generateAPIUrl = (formData:JQuery.NameValuePair[]) => {
+    let baseURL = "https://opentdb.com/api.php?";
+    const amount = getAmount(formData[0].value);
+    const category = getCategory(formData[1].value);
+    const difficulty = getDifficulty(formData[2].value);
+    const type = getType(formData[3].value);
     
+    const url = baseURL + amount + category + difficulty + type;
+    return url;
+}
+
+const attachFormEventHandler = () => {
+    const form = $("form");
+    form.on("submit", (event) => {
+        event.preventDefault();
+        const formData = form.serializeArray();
+        const url = generateAPIUrl(formData)
+        console.log(url)
+    })
 }
 
 
-
 populateCategoryDropdown();
+attachFormEventHandler();
