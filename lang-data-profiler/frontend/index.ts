f47eecf11918947c90769e7187542013397a279b
eric-sclafani <eric.sclafani321@gmail.com>
2024-07-13 16:56:41 -0400

    got frontend-backend connection working

--- lang-data-profiler/frontend/index.ts
+++ lang-data-profiler/frontend/index.ts
@@ -1,16 +1,30 @@
-async function getData() {
-  const url = "http://localhost:8000/lower/HELLO";
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const json = await response.json();
-    console.log(json);
-  } catch (error) {
-
-  }
+interface DataResponse {
+    lowered_text:string;
 }
 
-getData();
+
+
+class DataRequest {
+
+    data!:DataResponse
+
+    constructor(url:string){
+        this.fetchData(url);
+    }
+
+    private async fetchData(url:string):Promise<void>{
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+              throw new Error(`Response status: ${response.status}`);
+            }
+            const json = await response.json();
+            console.log(json);
+            this.data = json;
+          } catch (error) {
+                console.log(error)
+          }
+    }
+}
+
+const request = new DataRequest("http://localhost:8000/lower/HELLO");
